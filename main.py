786fc03197e61ba8a4c039d6622c955247b83906
Bno <Breno_live2005@hotmail.com>
2025-06-29 21:05:35 -0300

    feature/banco-de-dados

--- main.py
+++ main.py
@@ -17,7 +17,7 @@
 # Vamos acessar nossos endpoints
 # E vamos acessar os PATHs desses endpoints
 # Query strings são o "?" depois do path
-# o "&" funciona que nem o "and" da programação ]
+# o "&" funciona que nem o "and" da programação
 # http://127.0.0.1:8000/adicionar?id=1&nome=Harry Potter&autor=J.K&ano=2005
 # http://127.0.0.1:8000/livros
 # http://127.0.0.1:8000/atualizar/id?
@@ -30,6 +30,16 @@ from pydantic import BaseModel
 from typing import Optional
 import secrets
 import os
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, session
+
+DATABASE_URL = "sqlite:///./livros.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
 app = FastAPI(
     title="API de livros",
@@ -44,15 +54,34 @@ app = FastAPI(
 MEU_USUARIO = "admin"
 MINHA_SENHA = "admin"
 
+#inicia o import de segurança
 security = HTTPBasic()
 
-livros = {}
+livros_antigo = {}
 
+#modula a base do banco de dados
+class LivroDB(Base):
+    __tablename__ = "Livros"
+    id = Column(Integer, primary_key=True, index=True)
+    nome_livro = Column(String, index=True)
+    autor_livro = Column(String, index=True)
+    ano_livro = Column(Integer)
+ 
 class Livro(BaseModel):
     nome_livro: str
     autor_livro: str
     ano_livro: int
 
+Base.metadata.create_all(bind=engine)
+
+def sessao_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#Essa função valida usuario e senha
 def autenticar_usuario(credentials: HTTPBasicCredentials = Depends(security)):
     is_username_correct = secrets.compare_digest(credentials.username, MEU_USUARIO)
     is_password_correct = secrets.compare_digest(credentials.password, MINHA_SENHA)
@@ -66,53 +95,62 @@ def autenticar_usuario(credentials: HTTPBasicCredentials = Depends(security)):
 
 
 @app.get("/livros")
-def get_livros(page: int = 1, limit: int = 10, credentials: HTTPBasicCredentials = Depends(autenticar_usuario)):
+def get_livros(page: int = 1, limit: int = 10, db: session = Depends(sessao_db), credentials: HTTPBasicCredentials = Depends(autenticar_usuario)):
     if page < 1 or limit < 1:
         raise HTTPException(status_code=400, detail="Page ou limite com valores inválidos")
+
+    livros = db.query(LivroDB).offset((page - 1) * limit).limit(limit).all()
     
     if not livros:
         return {"message": "Não existe nenhum livro"}
 
-    livros_ordenados = sorted(livros.items(), key=lambda x: x[0])
-
-    start = (page - 1) * limit
-    end = start + limit
-
-    livros_paginados = [
-        {"id": id_livro, "nome_livro": livro_data["nome_livro"], "autor_livro": livro_data["autor_livro"], "ano_livro": livro_data["ano_livro"]}
-        for id_livro, livro_data in livros_ordenados[start:end]
-    ]
+    total_livros = db.query(LivroDB).count()
 
     return {
         "page": page,
         "limit": limit,
-        "total": len(livros),
-        "livros": livros_paginados
+        "total": total_livros,
+        "livros": [{"id": livro.id, "nome_livro": livro.nome_livro, "auto_livro": livro.autor_livro, "ano_livro": livro.ano_livro} for livro in livros]
     }
 
 @app.post("/adicionar")
-def post_livros(id: int, livro: Livro, credentials: HTTPBasicCredentials = Depends(autenticar_usuario)):
-    if id in livros:
-        raise HTTPException(status_code=400, detail="Esse livro já existe")
-    else:
-        livros[id] = livro.dict()
-        return {"message": "O livro foi criado com sucesso"}
+def post_livros(livro: Livro, db: session = Depends(sessao_db), credentials: HTTPBasicCredentials = Depends(autenticar_usuario)):
+   db_livro = db.query(LivroDB).filter(LivroDB.nome_livro == livro.nome_livro, LivroDB.autor_livro == livro.autor_livro).first()
+   if db_livro:
+       raise HTTPException(status_code=400, detail="Esse livro ja existe no banco de dados") 
+   
+   novo_livro = LivroDB(nome_livro=livro.nome_livro, autor_livro=livro.autor_livro, ano_livro=livro.ano_livro)
+   db.add(novo_livro)
+   db.commit()
+   db.refresh(novo_livro)
+
+   return{"message": "O livro foi criado com sucesso!"}
     
 @app.put("/atualizar/{id}")
-def put_livros(id: int, livro: Livro, credentials: HTTPBasicCredentials = Depends(autenticar_usuario)):
-    meu_livro = livros.get(id)
-    if id not in livros:
-        raise HTTPException(status_code=404, detail="Esse livro não foi encontrado")
-    else:
-        livros[id] = livro.dict()
-        return {"Message": "As informações do seu livro foram atualizadas com sucesso"}
+def put_livros(id: int, livro: Livro, db: session = Depends(sessao_db), credentials: HTTPBasicCredentials = Depends(autenticar_usuario)):
+   db_livro= db.query(LivroDB).filter(LivroDB.id == id).first()
+   if not db_livro:
+       raise HTTPException(status_code=404, detail="Este livro não foi encontrado no banco de dados")
+   
+   db_livro.nome_livro = livro.nome_livro
+   db_livro.autor_livro = livro.autor_livro
+   db_livro.ano_livro = livro.ano_livro
+   db.commit()
+   db.refresh(db_livro)
+
+   return {"message": "O livro foi atualizado com sucesso"}
     
 @app.delete("/deletar/{id}")
-def delete_livros(id: int, credentials: HTTPBasicCredentials = Depends(autenticar_usuario)):
-    if id not in livros:
-        raise HTTPException(status_code=404, detail="Esse livro não foi encontrado")
-    else:
-        del livros[id]
-        return {"message": "O livro foi deletado"}
+def delete_livros(id: int, db: session = Depends(sessao_db), credentials: HTTPBasicCredentials = Depends(autenticar_usuario)):
+    db_livro = db.query(LivroDB).filter(LivroDB.id == id).first()
+
+    if not db_livro:
+        raise HTTPException(status_code=404, detail="Este livro não foi encontrado no banco de dados")
     
+    db.delete(db_livro)
+    db.commit()
+
+    return{"message":"Seu livro foi deletado com sucesso"}
+
+
 #testa no postman ou inmsonia
